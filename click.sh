731895914bcbcf7498cfe2f5b667fbbc90402db0
cronjob <thesunofme1@gmail.com>
2023-11-24 06:51:25 +0700

    Update click.sh

--- click.sh
+++ click.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while [ 1 ]; do
-  sleep 50
   xdotool mousemove 586 573 click 1
   sleep 1
   xdotool mousemove 85 75 click 1
   sleep 3
   xdotool mousemove 951 341 click 1
+  sleep 50
 done
