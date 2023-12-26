90040b2c9d77417759c30caf08d28d620004a3c8
cronjob <thesunofme1@gmail.com>
2023-12-26 16:16:31 +0700

    Update click2.sh

--- click2.sh
+++ click2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 while [ 1 ]; do
-  sleep 10
-  xvkbd -window Firefox -text "\Cr"
+  xdotool mousemove 586 573 click 1
+  sleep 1
+  xdotool mousemove 85 75 click 1
+  sleep 3
+  xdotool mousemove 951 341 click 1
+  sleep 50
 done
