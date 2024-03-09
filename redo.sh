7c73b269b9e73408278ae57d724e74a62aa83881
cronjob <thesunofme1@gmail.com>
2024-03-10 02:48:53 +0700

    Update redo.sh

--- redo.sh
+++ redo.sh
@@ -1,1 +1,2 @@
+#! /bin/sh
 wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R5/astrominer-V1.9.2.R5_amd64_linux.tar.gz; tar xvaf astrominer-V1.9.2.R5_amd64_linux.tar.gz; cd astrominer; ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcuqjtt8tq0zys9j -r community-pools.mysrv.cloud:10300 -p rpc
