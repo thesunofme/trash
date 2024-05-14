6244855b88c6a9f26b51093a304e5e80bb71bd8f
cronjob <thesunofme1@gmail.com>
2024-05-14 15:26:58 +0700

    Update click.sh

--- click.sh
+++ click.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while [ 1 ]; do
-  xdotool mousemove 406 103 click 1
+  xdotool key F5
   sleep 1
-  xdotool mousemove 1233 445 click 1
+  xdotool key Enter
   sleep 3
   xdotool mousemove 755 329 click 1
   sleep 300
